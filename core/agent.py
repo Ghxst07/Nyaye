32e16e79ee6abe42f7535958b830f4933db8f3a9
Arindol Sarkar <arindolsarkar07@gmail.com>
2026-02-05 12:06:13 +0530

    Changed some logics

--- core/agent.py
+++ core/agent.py
@@ -2,24 +2,23 @@ import random
 from core.llm_agent import llm_generate
 
 def agent_decide_reply(session):
-    """
-    Decides the next move based on what data has been extracted so far.
-    Prioritizes: Link > UPI > Phone > Stall/Reassure Mix
-    """
+    
+    if session.extracted.get("upiIds"):
+        return llm_generate("upi_not_working", session.messages)
 
-    if not session.extracted.get("phishingLinks"):
-        return llm_generate("ask_for_phishing_link", session.messages)
+    
+    if session.extracted.get("phishingLinks"):
+        return llm_generate("link_not_working", session.messages)
 
-    if not session.extracted.get("upiIds"):
-        return llm_generate("ask_for_upi", session.messages)
+    
+    if session.extracted.get("phoneNumbers"):
+        return llm_generate("phone_not_reachable", session.messages)
 
-    if not session.extracted.get("phoneNumbers"):
-        return llm_generate("ask_for_phone", session.messages)
-
+    
     if random.random() < 0.3:
         return llm_generate("reassure", session.messages)
     
-    return llm_generate("stall", session.messages)
+    return llm_generate("ask_for_phishing_link", session.messages)
 
 
 def should_stop(session):
